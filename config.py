c810fbe02ab4ef2077454490380ccea9342cb69d
Alex Eshoo <AlexEshoo@gmail.com>
2020-06-11 20:18:28 -0400

    made session and remmeber cookies HTTPONLY

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_REFRESH_EACH_REQUEST = True
 
 
 class DevelopmentConfig(Config):
